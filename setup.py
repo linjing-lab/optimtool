15ebe54d91d9bd277b320e6e2b001aeb2d012c7f
DeeGLMath <1439313331@qq.com>
2021-12-28 23:53:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,13 @@ setup(
       author='林景',
       author_email='1439313331@qq.com',
       url="https://gitcode.net/linjing_zyq/optimtool",
-      packages=["optimtool", "optimtool.unconstrain", "optimtool.constrain", "optimtool.hybrid", "optimtool.example"], 
+      packages=[
+            "optimtool", 
+            "optimtool.unconstrain", 
+            "optimtool.constrain", 
+            "optimtool.hybrid", 
+            "optimtool.example"
+      ], 
       include_package_data=True,    # 自动打包文件夹内所有数据
       zip_safe=True, # 设定项目包为安全，不用每次都检测其安全性
       install_requires = [
