dc669aacfd79fcaf08319655d31cfef3f2dc4065
DeeGLMath <1439313331@qq.com>
2021-12-28 23:48:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
  
 setup(
       name='optimtool',  # 包名
@@ -7,6 +7,12 @@ setup(
       author='林景',
       author_email='1439313331@qq.com',
       url="https://gitcode.net/linjing_zyq/optimtool",
-      packages=["optimtool", "optimtool.unconstrain", "optimtool.constrain", "optimtool.hybrid", "optimtool.example", "functions"], 
-      install_requires=['numpy', 'matplotlib', 'sympy'],
+      packages=["optimtool", "optimtool.unconstrain", "optimtool.constrain", "optimtool.hybrid", "optimtool.example"], 
+      include_package_data=True,    # 自动打包文件夹内所有数据
+      zip_safe=True, # 设定项目包为安全，不用每次都检测其安全性
+      install_requires = [
+            'numpy',
+            'sympy',
+            'matplotlib',
+      ],
 )
