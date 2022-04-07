0ebe1dde61b5130cbe27b70bc0d834deb45679cb
linjing-lab <1439313331@qq.com>
2022-04-07 22:32:31 +0800

    modified test.py

--- test.py
+++ test.py
@@ -1,15 +1,32 @@
-# 导包
 import sympy as sp
 import matplotlib.pyplot as plt
-import optimtool.unconstrain as ou
+import optimtool as oo
 
-# 构造数据
-m = 1
-n = 2
-a = 0.2
-b = -1.4
-c = 2.2
-x3 = 2*(1/2)
-y3 = 0
-x_0 = (0, -1, -2.5, -0.5, 2.5, -0.05)
-oo.example.WanYuan.gauss_newton(1, 2, 0.2, -1.4, 2.2, 2**(1/2), 0, (0, -1, -2.5, -0.5, 2.5, -0.05), draw=True)
+f, x1, x2, x3, x4 = sp.symbols("f x1 x2 x3 x4")
+f = (x1 - 1)**2 + (x2 - 1)**2 + (x3 - 1)**2 + (x1**2 + x2**2 + x3**2 + x4**2 - 0.25)**2
+funcs = sp.Matrix([f])
+args = sp.Matrix([x1, x2, x3, x4])
+x_0 = (1, 2, 3, 4)
+
+f_list = []
+title = ["gradient_descent_barzilar_borwein", "newton_CG", "newton_quasi_L_BFGS", "trust_region_steihaug_CG"]
+colorlist = ["maroon", "teal", "slateblue", "orange"]
+_, _, f = oo.unconstrain.gradient_descent.barzilar_borwein(funcs, args, x_0, False, True)
+f_list.append(f)
+_, _, f = oo.unconstrain.newton.CG(funcs, args, x_0, False, True)
+f_list.append(f)
+_, _, f = oo.unconstrain.newton_quasi.L_BFGS(funcs, args, x_0, False, True)
+f_list.append(f)
+_, _, f = oo.unconstrain.trust_region.steihaug_CG(funcs, args, x_0, False, True)
+f_list.append(f)
+
+# draw
+handle = []
+for j, z in zip(colorlist, f_list):
+    ln, = plt.plot([i for i in range(len(z))], z, c=j, marker='o', linestyle='dashed')
+    handle.append(ln)
+plt.xlabel("$Iteration \ times \ (k)$")
+plt.ylabel("$Objective \ function \ value: \ f(x_k)$")
+plt.legend(handle, title)
+plt.title("Performance Comparison")
+plt.show()
