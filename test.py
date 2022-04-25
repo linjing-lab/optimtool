f915306e25ef437e7ee517cb0c11a9ed8f959017
林景 <82669431+linjing-lab@users.noreply.github.com>
2022-04-25 17:34:46 +0800

    modified test.py

--- test.py
+++ test.py
@@ -2,23 +2,22 @@ import sympy as sp
 import matplotlib.pyplot as plt
 import optimtool as oo
 
-f, x1, x2, x3, x4 = sp.symbols("f x1 x2 x3 x4")
-f = (x1 - 1)**2 + (x2 - 1)**2 + (x3 - 1)**2 + (x1**2 + x2**2 + x3**2 + x4**2 - 0.25)**2
-funcs = f # [f] \ (f) \ sp.Matrix([f])
+x1, x2, x3, x4 = sp.symbols("x1 x2 x3 x4")
+funcs = (x1 - 1)**2 + (x2 - 1)**2 + (x3 - 1)**2 + (x1**2 + x2**2 + x3**2 + x4**2 - 0.25)**2
 args = [x1, x2, x3, x4] # (x1, x2, x3, x4) \ sp.Matrix([args])
 x_0 = (1, 2, 3, 4)
 
 f_list = []
 title = ["gradient_descent_barzilar_borwein", "newton_CG", "newton_quasi_L_BFGS", "trust_region_steihaug_CG"]
 colorlist = ["maroon", "teal", "slateblue", "orange"]
-_, _, f = oo.unconstrain.gradient_descent.barzilar_borwein(funcs, args, x_0, False, True)
-f_list.append(f)
-_, _, f = oo.unconstrain.newton.CG(funcs, args, x_0, False, True)
-f_list.append(f)
-_, _, f = oo.unconstrain.newton_quasi.L_BFGS(funcs, args, x_0, False, True)
-f_list.append(f)
-_, _, f = oo.unconstrain.trust_region.steihaug_CG(funcs, args, x_0, False, True)
-f_list.append(f)
+_, _, fun = oo.unconstrain.gradient_descent.barzilar_borwein(funcs, args, x_0, False, True)
+f_list.append(fun)
+_, _, fun = oo.unconstrain.newton.CG(funcs, args, x_0, False, True)
+f_list.append(fun)
+_, _, fun = oo.unconstrain.newton_quasi.L_BFGS(funcs, args, x_0, False, True)
+f_list.append(fun)
+_, _, fun = oo.unconstrain.trust_region.steihaug_CG(funcs, args, x_0, False, True)
+f_list.append(fun)
 
 # draw
 handle = []
