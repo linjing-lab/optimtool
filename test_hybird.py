a3ee39e75ba7720341801353b9b13957db97b27d
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2023-11-05 11:56:21 +0800

    hybrid trial

--- test_hybird.py
+++ test_hybird.py
@@ -2,7 +2,7 @@ import optimtool.hybrid as oh
 from optimtool.base import sp
 x1, x2 = sp.symbols("x1 x2")
 obf = x1**2 + 2*x1*x2 + x2**2 + 2*x1 - 2*x2
-print(oh.nesterov.accer(obf, [x1, x2], (2, 3), lk=0.1, tk=0.2, verbose=True, proxim='ln', epsilon=4.7))
+print(oh.nesterov.accer(obf, [x1, x2], (2, 3), verbose=True, proxim='ln', epsilon=4.00501))
 # break criterion is chosen where the norm value of each gradient is less than epsilon in hybird
 # users need to set `epsilon` to a larger value in trials when encounter `RecursionError: maximum recursion depth exceeded in comparison`
 
