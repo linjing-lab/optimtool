bf267d4f71142c0ba5378eba66f6bcfe26739a86
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2023-11-05 16:09:53 +0800

    modify annotation

--- test_hybird.py
+++ test_hybird.py
@@ -5,7 +5,7 @@ obf = x1**2 + 2*x1*x2 + x2**2 + 2*x1 - 2*x2
 print(oh.nesterov.accer(obf, [x1, x2], (2, 3), verbose=True, proxim='ln', epsilon=4.00501))
 # break criterion is chosen where the norm value of each gradient is less than epsilon in hybird
 # users need to set `epsilon` to a larger value in trials when encounter `RecursionError: maximum recursion depth exceeded in comparison`
-# x_0 was renewed with rate set by tk*epsilon in iteration proccess, tk*epsilon ranges from 1e-3 and `_proxim/ln` contribute 1e-3 with tk * mu.
+# x_0 was renewed with rate set by tk*epsilon in iteration proccess, tk*epsilon ranges from 1e-3 and `_proxim/ln` contribute the precision of x_0 within 1e-3 with np.sqrt(tk * mu).
 
 '''
 $$
