625517df7825a9574c2dde608594cc0a72b5f43f
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2024-04-15 10:37:40 +0800

    correct noun

--- test_hybrid.py
+++ test_hybrid.py
@@ -3,7 +3,7 @@ from optimtool.base import sp
 x1, x2 = sp.symbols("x1 x2")
 obf = x1**2 + 2*x1*x2 + x2**2 + 2*x1 - 2*x2
 print(oh.nesterov.accer(obf, [x1, x2], (2, 3), verbose=True, proxim='ln', epsilon=4.00501))
-# break criterion is chosen where the norm value of each gradient is less than epsilon in hybird
+# break criterion is chosen where the norm value of each gradient is less than epsilon in hybrid
 # users need to set `epsilon` to a larger value in trials when encounter `RecursionError: maximum recursion depth exceeded in comparison`
 # x_0 was renewed with rate set by tk*epsilon in iteration proccess, tk*epsilon ranges from 1e-3 and `_proxim/ln` contribute the precision of `delta` within 1e-6 by tk*mu.
 
