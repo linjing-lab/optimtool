c7e1ac2260dde4d13a9acf9c5cc68a781dfd390e
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2024-04-18 16:43:55 +0800

    alter x_0

--- test_L_BFGS.py
+++ test_L_BFGS.py
@@ -5,7 +5,7 @@ f = 100 * (x[1] - x[0]**2)**2 + \
     (1 - x[0])**2 + \
     100 * (x[3] - x[2]**2)**2 + \
     (1 - x[2])**2
-x_0 = (-1.2, 1, -1.2, 1)
+x_0 = (-2, 0.5, -2, 0.5)
 lbfgs = ou.newton_quasi.L_BFGS
 lbfgs(f, x, x_0, verbose=True, draw=False, m=2)
 # lbfgs(f, x, x_0, verbose=True, draw=False, m=6)
