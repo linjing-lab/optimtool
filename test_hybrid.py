f616082407d61c58482ca3267263e53d1895a27c
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2024-04-27 17:24:21 +0800

    add case[FISTA_decline]

--- test_hybrid.py
+++ test_hybrid.py
@@ -1,16 +1,15 @@
 import optimtool.hybrid as oh
-from optimtool.base import sp
-
+from optimtool.base import sp 
 '''
 $$
 \min x^2+2xy+y^2+2x-2y \\
 \mathrm{s.t.} x \geq 0, y \geq 0
 $$
 '''
-
 x1, x2 = sp.symbols("x1 x2")
 obf = x1**2 + 2*x1*x2 + x2**2 + 2*x1 - 2*x2
 print(oh.nesterov.accer(obf, [x1, x2], (2, 3), verbose=True, proxim='ln', epsilon=4.00501))
+# print(oh.fista.decline(obf, [x1, x2], (2, 3), verbose=True, proxim='ln', epsilon=4.00501))
 # break criterion is chosen where the norm value of each gradient is less than epsilon in hybrid
 # users need to set `epsilon` to a larger value in trials when encounter `RecursionError: maximum recursion depth exceeded in comparison`
 # x_0 was renewed with rate set by tk*epsilon in iteration proccess, tk*epsilon ranges from 1e-3 and `_proxim/ln` contribute the precision of `delta` within 1e-6 by tk*mu.
