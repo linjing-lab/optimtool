2e0d1891eedab48dc278dd4f57558fce2e282720
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2025-02-11 13:10:49 +0800

    acc_function test_acc

--- test_acc.py
+++ test_acc.py
@@ -12,7 +12,7 @@ f5 = (sp.exp(x[0]) - x[0]) + (sp.exp(x[1]) - 2 * x[1]) + (sp.exp(x[2]) - 3 * x[2
 # add fit function into acc_data with collections as functions, initial starts, epsilons for multiple convergence conditions. 
 acc_data = [(f1, (-2, 2, -2, 2), 1e-1), (f2, (2, 2, 2, 2), 1e-2), (f3, (-1, 0.5, -1, 0.5), 1e-2), (f4, (5, 5, 5, 5), 1e-1), (f5, (0.5, 0.5, 0.5, 0.5), 1e-2)]
 
-acc_function = optimtool.unconstrain.newton_quasi.bfgs
+acc_function = optimtool.unconstrain.newton_quasi.bfgs # plot with full target description on third parameters in plot_iteration
 
 Parallel(n_jobs=-1, backend="loky", prefer="processes")(delayed(acc_function)(f[0], x, f[1], epsilon=f[2]) for f in acc_data)
-# Parallel(n_jobs=-1, backend="threading", prefer="processes")(delayed(acc_function)(f, x, (10, 10, 10, 10), draw=False, epsilon=1e-4) for f in acc_data)
+# Parallel(n_jobs=-1, backend="threading", prefer="processes")(delayed(acc_function)(f[0], x, f[1], draw=False, epsilon=f[2]) for f in acc_data)
