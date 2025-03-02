533822869ef214cc5aa6b80918c78f032fecfee4
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2025-03-02 15:48:07 +0800

    add indication[k-m, k-1]

--- test_L_BFGS.py
+++ test_L_BFGS.py
@@ -7,5 +7,5 @@ f = 100 * (x[1] - x[0]**2)**2 + \
     (1 - x[2])**2
 x_0 = (-2, 0.5, -2, 0.5)
 lbfgs = ou.newton_quasi.L_BFGS
-lbfgs(f, x, x_0, verbose=True, draw=False, m=2)
+lbfgs(f, x, x_0, verbose=True, draw=False, m=2)# m is history length as [k-m, k-1] in k iterations.
 # lbfgs(f, x, x_0, verbose=True, draw=False, m=6)
